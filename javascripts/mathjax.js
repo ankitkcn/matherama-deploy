721a1d94700cff79d582c5ee157ae81fa691764b
Ankit Kumar Chauhan <ankitchauhan.iitkgp@gmail.com>
2024-04-09 19:13:54 +0530

    The first Launch

---
+++ javascripts/mathjax.js
@@ -0,0 +1,20 @@
+window.MathJax = {
+    loader: {load: ['[tex]/cancel']},
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      displayMath: [['$$', '$$'], ['\\[', '\\]']],
+      processEscapes: true,
+      processEnvironments: true,
+      
+      packages: {'[+]': ['cancel']}
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+  document$.subscribe(() => { 
+    MathJax.typesetPromise()
+  })
+
